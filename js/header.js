9400da3772867cf6e1bc3a7794b939ce6e0d2b0d
Davide Raffaele <70845116+Kheeto@users.noreply.github.com>
2026-02-25 17:42:58 +0100

    Shared header
    Implemented a header loader script for modularity

---
+++ js/header.js
@@ -0,0 +1,33 @@
+function loadHeader() {
+    const depth = (window.location.pathname.match(/\//g) || []).length - 1;
+    const logoPath = '../'.repeat(depth) + 'immagini/logo.png';
+    const problemiPath = '../'.repeat(depth) + 'problemi/index.html';
+    const homePath = '../'.repeat(depth) + 'index.html';
+
+    document.getElementsByTagName("header")[0].innerHTML = `
+        <nav class="nav-container">
+        <div class="logo">
+            <img src="${logoPath}" alt="Logo">
+        </div>
+
+        <ul class="nav-links sans">
+            <li><a href="${homePath}">Home</a></li>
+            <li><a href="${problemiPath}">Problemi</a></li>
+        </ul>
+        </nav>
+    `;
+
+    // login button (coming soon)
+    /*
+        <nav class="nav-container">
+        <ul class="nav-links sans">
+            <a href="../login/index.html">
+                <div class="logo logo--login">
+                    <img src="../immagini/login.png" alt="Login">
+                </div>
+            </a>
+        </ul>
+        </nav>
+    `;
+    */
+}
