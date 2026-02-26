a1360d18489c0d61641c0425ad416b89dcb86c8e
Davide Raffaele <70845116+Kheeto@users.noreply.github.com>
2026-02-26 21:40:24 +0100

    CalcoBadge
    HTML and CSS for the implementation of CalcoBadge.

--- js/header.js
+++ js/header.js
@@ -4,7 +4,9 @@ function loadHeader() {
 
     const logoPath = base + "/immagini/logo.png";
     const problemiPath = base + "/problemi/index.html";
+    const badgePath = base + "/calcobadge/index.html";
     const homePath = base + "/index.html";
+    const loginPath = base + "/login/index.html";
 
     document.getElementsByTagName("header")[0].innerHTML = `
         <nav class="nav-container">
@@ -15,18 +17,20 @@ function loadHeader() {
         <ul class="nav-links sans">
             <li><a href="${homePath}">Home</a></li>
             <li><a href="${problemiPath}">Problemi</a></li>
+            <li><a href="${badgePath}">CalcoBadge</a></li>
         </ul>
         </nav>
 
-
         <nav class="nav-container">
         <ul class="nav-links sans">
-            <a href="../login/index.html">
-                <div class="logo logo--login">
-                    <img src="../immagini/login.png" alt="Login">
-                </div>
-            </a>
+            <li><a href="${loginPath}">Accedi / Registrati</a></li>
         </ul>
+
+        <a href="${loginPath}">
+        <div class="logo logo--login">
+            <img src="${base}/immagini/login.png" alt="Login">
+        </div>
+        </a>
         </nav>
     `;
 }
